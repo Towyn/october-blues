f5b204e7bfc1916907efcbeaa0656103eb269fff
Towyn <Towyn@users.noreply.github.com>
2023-10-29 01:55:48 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Towyn"
+original_name="october_blues"
+original_urlname="october-blues"
+original_description="Awesome october_blues created by Towyn"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv october_blues $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
